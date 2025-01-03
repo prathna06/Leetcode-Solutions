0a0e6cb531aecfac19f59b80f3958dedc38e52d6
Prathna Gunawat <32262051+prathna06@users.noreply.github.com>
2025-01-03 12:16:16 -0800

    Time: 8120 ms (5.02%), Space: 17.6 MB (38.69%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mp1={}
+        mp2={}
+        flag=True
+        for i in range(len(ransomNote)):
+            mp1[ransomNote[i]]=ransomNote.count(ransomNote[i])
+        for j in range(len(magazine)):
+            mp2[magazine[j]]=magazine.count(magazine[j])
+        
+        for k in mp1:
+            if k not in mp2 or mp1[k]>mp2[k]:
+                flag =False
+        if flag == True:
+            return True
+        else:
+            return False
+                
+            
+            #k-> ele
+            #mp1[k]-> value
+                

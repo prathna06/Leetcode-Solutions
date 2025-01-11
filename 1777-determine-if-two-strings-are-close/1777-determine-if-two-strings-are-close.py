441e42d09793b3a606fba4e3efab4c07561c061b
Prathna Gunawat <32262051+prathna06@users.noreply.github.com>
2025-01-11 15:18:42 -0800

    Time: 78 ms (90.26%), Space: 18.4 MB (12.09%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -1,15 +1,18 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        # print(Counter(word1).values())
 
-        if len(word1) != len(word2):
-            return False
-        wordBank1 = { c:0 for c in word1}
-        wordBank2 = {c:0 for c in word2}
-        for c in word1:
-            wordBank1[c] += 1
-        for c in word2:
-            wordBank2[c] += 1
- 
-        return wordBank1.keys() == wordBank2.keys() and sorted(wordBank1.values()) == sorted(wordBank2.values())
+        # print(set(word2))
+        # if len(word1) != len(word2):
+        #     return False
+        # wordBank1 = { c:0 for c in word1}
+        # wordBank2 = {c:0 for c in word2}
 
-       
+        # for c in word1:
+        #     wordBank1[c] += 1
+        # for c in word2:
+        #     wordBank2[c] += 1
+    
+        # return wordBank1.keys() == wordBank2.keys() and sorted(wordBank1.values()) == sorted(wordBank2.values())
+
+        return set(word1) == set(word2) and sorted(Counter(word1).values()) == sorted(Counter(word2).values())

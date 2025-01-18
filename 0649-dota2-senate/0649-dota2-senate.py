a02e18a41ed1e2db3c768feb586a8cc39a9c11cc
Prathna Gunawat <32262051+prathna06@users.noreply.github.com>
2025-01-18 00:10:24 -0800

    Time: 15 ms (48.06%), Space: 18.1 MB (43.32%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,24 +1,16 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        n = len(senate)
-        s, banned = set(), [False] * n
-        ban_d = ban_r = 0
-        while len(s) != 1:
-            s = set()
-            for i, p in enumerate(senate):
-                if banned[i]: continue
-                if p == 'R':
-                    if ban_r > 0:           # current R being banned
-                        ban_r -= 1
-                        banned[i] = True
-                    else:                   # if current R is valid, it will ban D
-                        ban_d += 1
-                        s.add('R')
-                else:        
-                    if ban_d > 0:           # current D being banned
-                        ban_d -= 1
-                        banned[i] = True
-                    else:                   # if current D is valid, it will ban R
-                        ban_r += 1
-                        s.add('D')
-        return 'Radiant' if s.pop() == 'R' else 'Dire'
+        # O(n) time and space
+        r_positions = deque([i for i, s in enumerate(senate) if s == 'R'])
+        d_positions = deque([i for i, s in enumerate(senate) if s == 'D'])
+        next_position = len(senate)
+        while r_positions and d_positions:
+            if r_positions[0] < d_positions[0]:
+                r_positions.append(next_position)
+            else:
+                d_positions.append(next_position)
+            r_positions.popleft()
+            d_positions.popleft()
+            next_position += 1
+        return 'Radiant' if r_positions else 'Dire'
+        

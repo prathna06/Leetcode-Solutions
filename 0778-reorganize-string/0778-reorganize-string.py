3e170ee075e7ef8217d213f22e75ab0a595b19a6
Prathna Gunawat <32262051+prathna06@users.noreply.github.com>
2025-03-14 18:16:50 -0700

    Time: 11 ms (5.12%), Space: 17.8 MB (75.27%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -10,15 +10,13 @@ class Solution:
             if (not new_string or new_string[-1] != keys[0]) and char_counts[keys[0]] > 0:
                 new_string += keys[0]
                 char_counts[keys[0]] -= 1
-                # if char_counts[keys[0]] < char_counts[keys[1]]:
-                char_counts = dict(sorted(char_counts.items(), key=lambda item: -item[1]))
+                if len(keys) > 1 and char_counts[keys[0]] < char_counts[keys[1]]:
+                    char_counts = dict(sorted(char_counts.items(), key=lambda item: -item[1]))
             elif len(keys) < 2 or char_counts[keys[1]] == 0:
                 return ""
             else:
                 new_string += keys[1]
                 char_counts[keys[1]] -= 1
-                # if char_counts[keys[1]] < char_counts[keys[2]]:
-                char_counts = dict(sorted(char_counts.items(), key=lambda item: -item[1]))
+                if len(keys) > 2 and char_counts[keys[1]] < char_counts[keys[2]]:
+                    char_counts = dict(sorted(char_counts.items(), key=lambda item: -item[1]))
         return new_string
-
-

18ba3c54380067bf6ff7d2688ea6a2b25191f627
Prathna Gunawat <32262051+prathna06@users.noreply.github.com>
2025-03-14 18:14:30 -0700

    Time: 31 ms (5.12%), Space: 18 MB (29.26%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        char_counts = Counter(s)
+        char_counts = dict(sorted(char_counts.items(), key=lambda item: -item[1]))
+        new_string = ""
+        for i in range(len(s)):
+            keys = list(char_counts.keys())
+            print(char_counts)
+            if (not new_string or new_string[-1] != keys[0]) and char_counts[keys[0]] > 0:
+                new_string += keys[0]
+                char_counts[keys[0]] -= 1
+                # if char_counts[keys[0]] < char_counts[keys[1]]:
+                char_counts = dict(sorted(char_counts.items(), key=lambda item: -item[1]))
+            elif len(keys) < 2 or char_counts[keys[1]] == 0:
+                return ""
+            else:
+                new_string += keys[1]
+                char_counts[keys[1]] -= 1
+                # if char_counts[keys[1]] < char_counts[keys[2]]:
+                char_counts = dict(sorted(char_counts.items(), key=lambda item: -item[1]))
+        return new_string
+
+
